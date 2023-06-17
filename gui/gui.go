44e02d24bd3ac9a229302deda3bc68ea18bfd94c
Bogdan Bojan <bogdanbojan03@gmail.com>
2023-06-17 17:28:03 +0000

    Add preliminary split of the gui window

--- gui/gui.go
+++ gui/gui.go
@@ -3,6 +3,7 @@
 package gui
 
 import (
+	"fmt"
 	"io"
 
 	"fyne.io/fyne/v2"
@@ -14,7 +15,8 @@ import (
 
 type gui struct {
 	content *widget.Entry
-	uri     fyne.URI
+	//	list    *widget.List
+	uri fyne.URI
 
 	win fyne.Window
 }
@@ -33,13 +35,10 @@ func ShowGUI() {
 	w := a.NewWindow("Loci")
 	g := newGUI(w)
 
-	hello := widget.NewLabel("Hello Fyne!")
+	hello := widget.NewLabel("Hello!")
 	g.win.SetContent(container.NewVBox(
 		hello,
-		widget.NewButton("Hi!", func() {
-			hello.SetText("Welcome :)")
-		}),
-		widget.NewButton("Open File", func() {
+		widget.NewButton("Choose .txt file", func() {
 			g.openFile()
 		}),
 	))
@@ -47,6 +46,37 @@ func ShowGUI() {
 	w.Resize(fyne.NewSize(500, 320))
 	g.win.ShowAndRun()
 }
+
+//	func (g *gui) loadUI() fyne.CanvasObject {
+//		g.content = widget.NewMultiLineEntry()
+//		g.content.SetText(u.placeholderContent())
+//
+//		visible := g.notes.notes()
+//		if len(visible) > 0 {
+//			g.setNote(visible[0])
+//			g.list.Select(0)
+//		}
+//
+//		bar := widget.NewToolbar(
+//			widget.NewToolbarAction(theme.ContentAddIcon(), func() {
+//				g.addNote()
+//			}),
+//			widget.NewToolbarAction(theme.ContentRemoveIcon(), func() {
+//				g.removeCurrentNote()
+//			}),
+//		)
+//
+//		side := fyne.NewContainerWithLayout(layout.NewBorderLayout(bar, nil, nil, nil),
+//			bar, container.NewVScroll(g.list))
+//
+//		return newAdaptiveSplit(side, g.content)
+//	}
+//
+//	func newAdaptiveSplit(left, right fyne.CanvasObject) *fyne.Container {
+//		split := container.NewHSplit(left, right)
+//		split.Offset = 0.33
+//		return container.New(&adaptiveLayout{split: split}, split)
+//	}
 
 func (g *gui) openFile() {
 	dialog.ShowFileOpen(func(r fyne.URIReadCloser, err error) {
