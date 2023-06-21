3b1ed87f0cbcba18deba4494c0c95de8e041d637
Bogdan Bojan <bogdanbojan03@gmail.com>
2023-06-21 18:54:13 +0000

    Add preliminary search bar in the gui window

--- gui/gui.go
+++ gui/gui.go
@@ -5,17 +5,18 @@ package gui
 import (
 	"fmt"
 	"io"
+	"log"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
 type gui struct {
 	content *widget.Entry
-	//	list    *widget.List
 	uri fyne.URI
 
 	win fyne.Window
@@ -35,49 +36,25 @@ func ShowGUI() {
 	w := a.NewWindow("Loci")
 	g := newGUI(w)
 
+	input := widget.NewEntry()
+	input.SetPlaceHolder("Enter a word to search for...")
+
+	searchBox := container.NewVBox(input, widget.NewButtonWithIcon("Search", theme.SearchIcon(), func() {
+		log.Println("Content was: ", input.Text)
+	}))
+
 	hello := widget.NewLabel("Hello!")
 	g.win.SetContent(container.NewVBox(
 		hello,
 		widget.NewButton("Choose .txt file", func() {
 			g.openFile()
 		}),
+		searchBox,
 	))
 
 	w.Resize(fyne.NewSize(500, 320))
 	g.win.ShowAndRun()
 }
-
-//	func (g *gui) loadUI() fyne.CanvasObject {
-//		g.content = widget.NewMultiLineEntry()
-//		g.content.SetText(u.placeholderContent())
-//
-//		visible := g.notes.notes()
-//		if len(visible) > 0 {
-//			g.setNote(visible[0])
-//			g.list.Select(0)
-//		}
-//
-//		bar := widget.NewToolbar(
-//			widget.NewToolbarAction(theme.ContentAddIcon(), func() {
-//				g.addNote()
-//			}),
-//			widget.NewToolbarAction(theme.ContentRemoveIcon(), func() {
-//				g.removeCurrentNote()
-//			}),
-//		)
-//
-//		side := fyne.NewContainerWithLayout(layout.NewBorderLayout(bar, nil, nil, nil),
-//			bar, container.NewVScroll(g.list))
-//
-//		return newAdaptiveSplit(side, g.content)
-//	}
-//
-//	func newAdaptiveSplit(left, right fyne.CanvasObject) *fyne.Container {
-//		split := container.NewHSplit(left, right)
-//		split.Offset = 0.33
-//		return container.New(&adaptiveLayout{split: split}, split)
-//	}
-
 func (g *gui) openFile() {
 	dialog.ShowFileOpen(func(r fyne.URIReadCloser, err error) {
 		if err != nil {
