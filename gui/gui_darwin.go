5665be9599569e4f8573f5e654a5e8702ec6bafc
Bogdan Bojan <bogdanbojan03@gmail.com>
2023-07-25 06:53:52 +0000

    Update darwin and windows gui file

--- gui/gui_darwin.go
+++ gui/gui_darwin.go
@@ -4,12 +4,15 @@ package gui
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"net/url"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"github.com/bogdanbojan/macaw/api"
@@ -18,14 +21,40 @@ import (
 
 type gui struct {
 	search
-	win fyne.Window
+	dataFetchContainer *fyne.Container
+	tabs               *container.AppTabs
+	URI                fyne.URI
+	win                fyne.Window
 }
 
 type search struct {
-	entry        *widget.Entry
-	button       *widget.Button
+	entry  *widget.Entry
+	button *widget.Button
+	sources
+}
+
+type sources struct {
+	localDict
+	onlineDict
+	wikipedia
+}
+
+type localDict struct {
 	result       *widget.Label
 	resultScroll *container.Scroll
+	slider       *widget.Slider
+}
+
+type onlineDict struct {
+	result       *widget.Label
+	resultScroll *container.Scroll
+	slider       *widget.Slider
+}
+
+type wikipedia struct {
+	result       *widget.Label
+	resultScroll *container.Scroll
+	slider       *widget.Slider
 }
 
 func ShowGUI() {
@@ -51,14 +80,27 @@ func ShowGUI() {
 
 	g.search.button = widget.NewButtonWithIcon("Search", theme.SearchIcon(), nil)
 
-	g.search.result = widget.NewLabel("")
-	g.search.result.Wrapping = fyne.TextWrapWord
-	g.search.resultScroll = container.NewVScroll(g.search.result)
+	// TODO: Export this intialization somewhere else.
+	g.search.localDict.result = widget.NewLabel("")
+	g.search.localDict.result.Wrapping = fyne.TextWrapWord
+	g.search.localDict.resultScroll = container.NewVScroll(g.search.localDict.result)
 
-	// TODO: Implement the copy button later.
-	// contentCopyButton := widget.NewButtonWithIcon("Copy to clipboard", theme.ContentCopyIcon(), func() {
-	// 	g.win.Clipboard().SetContent(g.search.result.Text)
-	// })
+	g.search.onlineDict.result = widget.NewLabel("")
+	g.search.onlineDict.result.Wrapping = fyne.TextWrapWord
+	g.search.onlineDict.resultScroll = container.NewVScroll(g.search.onlineDict.result)
+
+	g.search.wikipedia.result = widget.NewLabel("")
+	g.search.wikipedia.result.Wrapping = fyne.TextWrapWord
+	g.search.wikipedia.resultScroll = container.NewVScroll(g.search.wikipedia.result)
+
+	tabLocalDict := container.NewTabItem("Local dictionary", g.search.localDict.resultScroll)
+	tabOnlineDict := container.NewTabItem("Online dictionary", g.search.onlineDict.resultScroll)
+	tabWiki := container.NewTabItem("Wikipedia", g.search.wikipedia.resultScroll)
+
+	g.tabs = container.NewAppTabs(tabLocalDict, tabOnlineDict, tabWiki)
+	g.tabs.DisableItem(tabOnlineDict)
+	g.tabs.DisableItem(tabWiki)
+	g.tabs.SetTabLocation(container.TabLocationTop)
 
 	g.search.entry.OnSubmitted = func(string) {
 		if len(g.search.entry.Text) == 0 {
@@ -66,31 +108,59 @@ func ShowGUI() {
 			return
 		}
 
-		// res, err := SearchWiki(g.search.entry.Text)
-		// if err != nil {
-		// 	g.search.result.SetText("Word not found")
-		// 	return
-		// }
+		if g.localDict.slider.Value == 1 {
+			g.tabs.EnableItem(g.tabs.Items[0])
 
-		def, err := api.HandleLocalResponse(g.search.entry.Text)
-		if err != nil || def == nil {
-			g.search.result.SetText("Word not found")
-			return
+			def, err := api.HandleLocalResponse(g.search.entry.Text)
+			if err != nil || def == nil {
+				g.search.localDict.result.SetText("Word not found")
+				return
+			}
+
+			var res string
+			for i, v := range def {
+				res += fmt.Sprintf("[%d] %s \n", i, v)
+			}
+
+			g.search.localDict.result.SetText(res)
+
 		}
 
-		var res string
-		for i, v := range def {
-			res += fmt.Sprintf("[%d] %s \n", i, v)
+		if g.onlineDict.slider.Value == 1 {
+			g.tabs.EnableItem(g.tabs.Items[1])
+
+			res, err := api.ApiRequest([]string{g.search.entry.Text})
+			if err != nil {
+				g.search.onlineDict.result.SetText("Word not found")
+				return
+			}
+
+			g.search.onlineDict.result.SetText(res[0])
 		}
 
-		g.search.result.SetText(res)
+		if g.wikipedia.slider.Value == 1 {
+			g.tabs.EnableItem(g.tabs.Items[2])
+
+			res, err := api.SearchWiki(g.search.entry.Text)
+			if err != nil {
+				g.search.wikipedia.result.SetText("Summary for word not found")
+				return
+			}
+			g.search.wikipedia.result.SetText(res)
+		}
+
+		g.tabs.Show()
 		fnResize()
 	}
 
+	toolbar := g.constructToolbar()
+	g.constructDataFetchContainer()
+	g.localDict.slider.SetValue(1)
+
 	g.win.SetContent(container.NewBorder(
-		g.search.entry,
-		nil, nil, nil,
-		g.search.resultScroll,
+		toolbar,
+		g.search.entry, nil, nil,
+		g.tabs,
 	))
 
 	go func() {
@@ -106,85 +176,110 @@ func ShowGUI() {
 			g.win.Canvas().Focus(g.search.entry)
 		}
 	}()
-
+	g.listenSliderChange()
 	fnResize()
-	g.win.Resize(fyne.NewSize(500, 150))
+	g.win.Resize(fyne.NewSize(500, 200))
 	g.win.ShowAndRun()
 }
 
-// TODO: Postpone the features below implementation until we solve the keyboard shortcut problem.
-// ===========================================================================
+func (g *gui) listenSliderChange() {
+	g.localDict.slider.OnChanged = func(f float64) {
+		if f == 0 {
+			g.tabs.DisableItem(g.tabs.Items[0])
+			return
+		}
+		g.tabs.EnableItem(g.tabs.Items[0])
+		g.tabs.SelectIndex(0)
+	}
 
-// func (g *gui) constructDataFetchContainer() {
-// 	// Data fetching options.
-// 	wikiLabel := widget.NewLabel("Wikipedia")
-// 	wikiSlider := widget.NewSlider(0, 1)
-// 	localDictLabel := widget.NewLabel("Local dictionary")
-// 	localDictSlider := widget.NewSlider(0, 1)
-// 	onlineDictLabel := widget.NewLabel("Online dictionary")
-// 	onlineDictSlider := widget.NewSlider(0, 1)
-//
-// 	dataFetchContainer := container.NewVBox()
-// 	dataFetchContainer.Add(widget.NewLabel("Data fetching options"))
-// 	dataFetchContainer.Add(widget.NewSeparator())
-// 	dataFetchContainer.Add(container.NewAdaptiveGrid(2, wikiLabel, wikiSlider))
-// 	dataFetchContainer.Add(container.NewAdaptiveGrid(2, localDictLabel, localDictSlider))
-// 	dataFetchContainer.Add(container.NewAdaptiveGrid(2, onlineDictLabel, onlineDictSlider))
-//
-// 	g.dataFetchContainer = dataFetchContainer
-// }
+	g.onlineDict.slider.OnChanged = func(f float64) {
+		if f == 0 {
+			g.tabs.DisableItem(g.tabs.Items[1])
+			return
+		}
+		g.tabs.SelectIndex(1)
+		g.tabs.EnableItem(g.tabs.Items[1])
+	}
 
-// func (g *gui) constructToolbar() *widget.Toolbar {
-// 	// Use container.NewTabItemWithIcon contained in a container.NewAppTabs
-// 	// if you want to add additional text to the icon.
-// 	toolbar := widget.NewToolbar()
-//
-// 	url, _ := url.Parse("https://github.com/bogdanbojan/macaw")
-// 	hyperlink := widget.NewHyperlink("Macaw github repository", url)
-//
-// 	chooseFileToolbar := widget.NewToolbarAction(theme.FolderOpenIcon(), func() {
-// 		g.openFile()
-// 	})
-// 	settingsToolbar := widget.NewToolbarAction(theme.SettingsIcon(), func() {
-// 		widget.ShowPopUpAtPosition(g.dataFetchContainer, g.win.Canvas(), fyne.NewPos(0, 40))
-// 	})
-// 	infoToolbar := widget.NewToolbarAction(theme.InfoIcon(), func() {
-// 		// dialog.ShowInformation("About", "https://github.com/bogdanbojan/macaw", w)
-// 		widget.ShowPopUpAtPosition(hyperlink, g.win.Canvas(), fyne.NewPos(0, 40))
-//
-// 	})
-// 	toolbar.Append(chooseFileToolbar)
-// 	toolbar.Append(settingsToolbar)
-// 	toolbar.Append(infoToolbar)
-//
-// 	return toolbar
-// }
-//
-// func (g *gui) openFile() {
-// 	dialog.ShowFileOpen(func(r fyne.URIReadCloser, err error) {
-// 		if err != nil {
-// 			dialog.ShowError(err, g.win)
-// 			return
-// 		}
-// 		if r == nil {
-// 			return
-// 		}
-// 		g.uri = r.URI()
-// 		g.loadFile(r)
-// 	}, g.win)
-// }
-//
-// func (g *gui) loadFile(r fyne.URIReadCloser) {
-// 	read, err := storage.Reader(g.uri)
-// 	if err != nil {
-// 		log.Println("Error opening resource", err)
-// 	}
-//
-// 	defer read.Close()
-// 	data, err := io.ReadAll(read)
-// 	if err == nil {
-// 		log.Println("Error reading data", err)
-// 	}
-//
-// 	fmt.Println(string(data))
-// }
+	g.wikipedia.slider.OnChanged = func(f float64) {
+		if f == 0 {
+			g.tabs.DisableItem(g.tabs.Items[2])
+			return
+		}
+		g.tabs.SelectIndex(2)
+		g.tabs.EnableItem(g.tabs.Items[2])
+	}
+
+}
+
+func (g *gui) constructDataFetchContainer() {
+	// Data fetching options.
+	localDictLabel := widget.NewLabel("Local dictionary")
+	g.localDict.slider = widget.NewSlider(0, 1)
+	onlineDictLabel := widget.NewLabel("Online dictionary")
+	g.onlineDict.slider = widget.NewSlider(0, 1)
+	wikiLabel := widget.NewLabel("Wikipedia")
+	g.wikipedia.slider = widget.NewSlider(0, 1)
+
+	dataFetchContainer := container.NewVBox()
+	dataFetchContainer.Add(widget.NewLabel("Data fetching options"))
+	dataFetchContainer.Add(widget.NewSeparator())
+	dataFetchContainer.Add(container.NewAdaptiveGrid(2, localDictLabel, g.localDict.slider))
+	dataFetchContainer.Add(container.NewAdaptiveGrid(2, onlineDictLabel, g.onlineDict.slider))
+	dataFetchContainer.Add(container.NewAdaptiveGrid(2, wikiLabel, g.wikipedia.slider))
+
+	g.dataFetchContainer = dataFetchContainer
+}
+
+func (g *gui) constructToolbar() *widget.Toolbar {
+	toolbar := widget.NewToolbar()
+
+	url, _ := url.Parse("https://github.com/bogdanbojan/macaw")
+	hyperlink := widget.NewHyperlink("Macaw github repository", url)
+
+	chooseFileToolbar := widget.NewToolbarAction(theme.FolderOpenIcon(), func() {
+		g.openFile()
+	})
+	settingsToolbar := widget.NewToolbarAction(theme.SettingsIcon(), func() {
+		widget.ShowPopUpAtPosition(g.dataFetchContainer, g.win.Canvas(), fyne.NewPos(0, 40))
+	})
+	infoToolbar := widget.NewToolbarAction(theme.InfoIcon(), func() {
+		widget.ShowPopUpAtPosition(hyperlink, g.win.Canvas(), fyne.NewPos(0, 40))
+
+	})
+	toolbar.Append(chooseFileToolbar)
+	toolbar.Append(settingsToolbar)
+	toolbar.Append(infoToolbar)
+
+	return toolbar
+}
+
+func (g *gui) openFile() {
+	dialog.ShowFileOpen(func(r fyne.URIReadCloser, err error) {
+		if err != nil {
+			dialog.ShowError(err, g.win)
+			return
+		}
+		if r == nil {
+			return
+		}
+		g.URI = r.URI()
+		g.loadFile(r)
+	}, g.win)
+}
+
+func (g *gui) loadFile(r fyne.URIReadCloser) {
+	read, err := storage.Reader(g.URI)
+	if err != nil {
+		log.Println("Error opening resource", err)
+	}
+
+	defer read.Close()
+	data, err := io.ReadAll(read)
+	if err == nil {
+		log.Println("Error reading data", err)
+	}
+
+	fmt.Println(string(data))
+}
+
